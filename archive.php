041ec75a6ffd0db32f5c3a8e80281644acd161c7
JB Christy <jbchrsty@stanford.edu>
2022-09-21 14:05:09 -0700

    get category template by slug, not id

--- archive.php
+++ archive.php
@@ -22,7 +22,7 @@ $context[ 'title' ] = get_the_archive_title();
 if ( is_post_type_archive() ) {
   array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
 } else if ( is_category() ) {
-	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
+	array_unshift( $templates, 'archive-' . get_query_var( 'category_name' ) . '.twig' );
 } else if ( is_tag() ) {
   array_unshift( $templates, 'archive-' . get_query_var( 'tag' ) . '.twig' );
 }
